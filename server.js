ba2ecabc41210c56e50a5efc66c581dace84cbe7
MiOzlv <mortegava0001@gmail.com>
2023-07-07 21:25:02 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+function bulb_on() {
+    document.getElementById('bulb').src='https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png';
+}
+function bulb_off() {
+    document.getElementById('bulb').src='https://freepngimg.com/save/31669-bulb-off-transparent-image/500x500';
+}
